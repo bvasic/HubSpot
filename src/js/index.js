c5a64259d59f4099a7a7b81e028a8bde2df9ecf0
bvasic <vasicbie@gmail.com>
2019-01-26 21:12:12 +0000

    Rendering index.ejs view on root GET request
    Also using Koala for compiling SCSS to css in bundle before integration of Webpack

--- src/js/index.js
+++ src/js/index.js
@@ -6,26 +6,29 @@ var form = require('express-form');
 var port = process.env.PORT || 7777;
 var jsonfile = require('jsonfile');
 var fs = require('fs');
-var file = './data/data.json';
 var path = require('path');
 var content = fs.readFileSync("./data/data.json");
 
 // Define to JSON type
 var jsonContent = JSON.parse(content);
 
-server.use(express.static(path.resolve('./bundle')));
-server.set('view engine', 'ejs');
-server.set('views', path.join(__dirname, '../../views/pages/'));
-
 server.use(bodyParser.urlencoded({extended: true}))
+server.use(express.static(__dirname+'./../views/'));
+server.use(express.static(path.resolve('./src/')));
+server.use(express.static(path.join('/')));
+
+server.set('views',path.join(__dirname+'./../views/pages'));
+server.set('view engine', 'ejs');
 
 server.listen(port, function(){
     console.log("Server running on port "+port);
     });
 
 //MAIN GET REQUEST
-server.get('/', (req, res) => {
-  	res.send(jsonContent);
+server.get('/', (req, res)=>{
+	//res.render('index');
+	res.render(__dirname+'/../views/pages/index.ejs',{media : jsonContent});
+  	//res.send(jsonContent);
   	//res.render('index.ejs',{media : jsonContent});
-  // render EJS template populated with data from data.json
+  	//render EJS template populated with data from data.json
 });
