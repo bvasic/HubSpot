0760d1b84a62d25911b93b08483fa71035537503
bvasic <vasicbie@gmail.com>
2019-01-26 23:53:55 +0000

    Passing json object to partial view

--- src/js/index.js
+++ src/js/index.js
@@ -7,12 +7,16 @@ var port = process.env.PORT || 7777;
 var jsonfile = require('jsonfile');
 var fs = require('fs');
 var path = require('path');
-var content = fs.readFileSync("./data/data.json");
+var file = './data/data.json';
+// jsonfile.readFile(file, function(err,obj){
+//     console.log(obj);
+// });
 
 // Define to JSON type
-var jsonContent = JSON.parse(content);
+// var jsonContent = JSON.parse(file);
 
 server.use(bodyParser.urlencoded({extended: true}));
+server.use(bodyParser.json());
 server.use(express.static(__dirname+'/../views/'));
 server.use(express.static(__dirname+'/../../src/bundle/'));
 server.use(express.static(path.join(__dirname, 'src')));
@@ -26,12 +30,30 @@ server.listen(port, function(){
     console.log("Server running on port "+port);
     });
 
+
+// server.get("/",function(req,res){
+//     jsonfile.readFile(file, function read(err, data) {
+//         if (err) {
+//             throw err;
+//         }
+//         var content = data;
+//         res.json(content);
+//     });    
+// });
 //MAIN GET REQUEST
-server.get('/', (req, res)=>{
-	console.log("Server running on port "+port);
+server.get('/',function(req,res){
 	console.log("get request sent");
+	var testing = jsonfile.readFile(file, function read(err, data) {
+        if (err) {
+            throw err;
+        }
+        var content = data;
+        res.json(content);
+    });  
+    var bla = JSON.stringify(testing);
+
 	//res.render('index');
-	res.render(__dirname+'/../views/pages/index.ejs',{data : jsonContent});
+	res.render(__dirname+'/../views/pages/index.ejs',bla);
   	//res.send(jsonContent);
   	//res.render('index.ejs',{media : jsonContent});
   	//render EJS template populated with data from data.json
